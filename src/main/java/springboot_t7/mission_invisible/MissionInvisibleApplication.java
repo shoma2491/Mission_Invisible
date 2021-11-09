a17071a7f433fb71ed4e4d5a8becc7ea57387626
shoma2491 <e1b19062@st.oit.ac.jp>
2021-11-09 16:26:18 +0900

    リポジトリの初期化

---
+++ src/main/java/springboot_t7/mission_invisible/MissionInvisibleApplication.java
@@ -0,0 +1,13 @@
+package springboot_t7.mission_invisible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionInvisibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionInvisibleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot_t7/mission_invisible/MissionInvisibleApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot_t7.mission_invisible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionInvisibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
